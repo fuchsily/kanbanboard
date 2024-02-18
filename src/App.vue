d36351a093bc92b7cb3e22ff95bb80abd2deee11
fuchsily <victoria.fuchs@arcor.de>
2024-02-18 17:33:19 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-4">
+        <div class="card">
+          <div class="card-header text-center bg-secondary text-white">
+            <h4>Neue Aufgaben</h4>
+          </div>
+          <div class="card-body">
+            <div
+              class="alert alert-secondary"
+              v-for="task in newTasks"
+              :key="task.id"
+            >
+              {{ task.content }}
+            </div>
+          </div>
+          <div class="card-footer">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Neue Aufgabe"
+            />
+            <div class="d-grid my-2">
+              <button class="btn btn-secondary">Eintragen</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="card">
+          <div class="card-header text-center bg-primary text-white">
+            <h4>In Bearbeitung</h4>
+          </div>
+          <div class="card-body"></div>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="card">
+          <div class="card-header text-center bg-success text-white">
+            <h4>Erledigt</h4>
+          </div>
+          <div class="card-body"></div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          content: "Dashboard überarbeiten.",
+          status: 0,
+        },
+        {
+          id: 2,
+          content: "Anwendung auf Vue.js umstellen.",
+          status: 0,
+        },
+      ],
+    };
+  },
+  computed: {
+    newTasks() {
+      return this.tasks.filter((task) => task.status === 0);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "~bootstrap/dist/css/bootstrap.min.css";
 </style>
